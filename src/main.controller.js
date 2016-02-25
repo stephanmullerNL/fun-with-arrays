671e41f62dc9f37d8201365754729b7f79164d8d
Stephan Muller <stephan.muller@luminis.eu>
2016-02-25 21:35:07 +0100

    Split up into cases, implemented case 1 (todo: stateful case switcher)

--- src/main.controller.js
+++ src/main.controller.js
@@ -3,32 +3,36 @@ var helpers = require('./helpers');
 MainController.$inject = ['$scope', '$sce', 'employeeService'];
 
 function MainController($scope, $sce, employeeService) {
+    var allEmployees;
 
-    function listEmployees() {
+    $scope.case1 = function() {
+        var employees = allEmployees;
 
-        employeeService.get().then(function (data) {
-            var employees;
-
-            // do some magic here
-            employees = data;
-
-
-            // display the result
-            $scope.employees = employees;
+        // do your magic here
 
 
 
+        // output the results
+        $scope.employees = employees;
 
+        // output the raw data
         $scope.prettyJson = $sce.trustAsHtml(helpers.prettyJson(employees));
 
+        // switch to the right view
+        $scope.case = 1;
+        console.log($scope.case);
+    };
 
 
 
 
+    function init() {
+        employeeService.get().then(function (data) {
+            allEmployees = data;
         });
     }
 
-    listEmployees();
+    init();
 }
 
 module.exports = MainController;
