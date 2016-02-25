d3897535ddc9527af4f3beb25ec684a667e76bd8
Stephan Muller <stephan.muller@luminis.eu>
2016-02-25 21:33:29 +0100

    Moved prettyJson to helpers, added more data

--- src/main.controller.js
+++ src/main.controller.js
@@ -1,3 +1,5 @@
+var helpers = require('./helpers');
+
 MainController.$inject = ['$scope', '$sce', 'employeeService'];
 
 function MainController($scope, $sce, employeeService) {
@@ -13,41 +15,16 @@ function MainController($scope, $sce, employeeService) {
 
             // display the result
             $scope.employees = employees;
-            $scope.prettyJson = $sce.trustAsHtml(highlightJson(employees));
-        });
-    }
 
 
-    // prettify the json data by adding some classes
-    // stolen from StackOverflow obviously
-    function highlightJson(json) {
-        var jsonRegex = /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g;
 
-        if (typeof json !== 'string') {
-            json = JSON.stringify(json, undefined, 2);
-        }
 
-        json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+        $scope.prettyJson = $sce.trustAsHtml(helpers.prettyJson(employees));
 
-        return json.replace(jsonRegex, function (match) {
-            var cls = 'number';
 
-            if (/^"/.test(match)) {
 
-                if (/:$/.test(match)) {
-                    cls = 'key';
-                } else {
-                    cls = 'string';
-                }
 
-            } else if (/true|false/.test(match)) {
-                cls = 'boolean';
 
-            } else if (/null/.test(match)) {
-                cls = 'null';
-            }
-
-            return '<span class="' + cls + '">' + match + '</span>';
         });
     }
 
